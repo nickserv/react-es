b2d895ed76e4fd1477c17174bb0e963790681e59
Nick McCurdy <nick@nickmccurdy.com>
2023-07-27 15:14:27 -0400

    Use `prettier-config-a11y`

--- App.js
+++ App.js
@@ -1,37 +1,37 @@
-import { createElement, useState } from "react"
+import { createElement, useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+	const [count, setCount] = useState(0);
 
-  return createElement(
-    "div",
-    { className: "App" },
-    createElement(
-      "a",
-      { href: "https://react.dev/", target: "_blank" },
-      createElement("img", {
-        src: "/react.svg",
-        className: "logo",
-        alt: "React logo",
-      }),
-    ),
-    createElement("h1", {}, "React"),
-    createElement(
-      "div",
-      { className: "card" },
-      createElement(
-        "button",
-        { onClick: () => setCount((count) => count + 1) },
-        "count is ",
-        count,
-      ),
-    ),
-    createElement(
-      "p",
-      { className: "read-the-docs" },
-      "Click on the React logo to learn more",
-    ),
-  )
+	return createElement(
+		"div",
+		{ className: "App" },
+		createElement(
+			"a",
+			{ href: "https://react.dev/", target: "_blank" },
+			createElement("img", {
+				src: "/react.svg",
+				className: "logo",
+				alt: "React logo",
+			}),
+		),
+		createElement("h1", {}, "React"),
+		createElement(
+			"div",
+			{ className: "card" },
+			createElement(
+				"button",
+				{ onClick: () => setCount((count) => count + 1) },
+				"count is ",
+				count,
+			),
+		),
+		createElement(
+			"p",
+			{ className: "read-the-docs" },
+			"Click on the React logo to learn more",
+		),
+	);
 }
 
-export default App
+export default App;
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-import { createElement, StrictMode } from "react"
-import ReactDOM from "react-dom/client"
-import App from "./App.js"
+import { createElement, StrictMode } from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  createElement(StrictMode, {}, createElement(App)),
-)
+	createElement(StrictMode, {}, createElement(App)),
+);
